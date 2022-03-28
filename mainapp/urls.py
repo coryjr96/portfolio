5b5edb7e8dac540aca81d6b11c8dd7a410a59309
Cory James Mitchell Jr <coryjr96@Sarahs-MacBook-Pro.local>
2022-03-27 22:33:58 -0400

    committed

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 #URLConf
 urlpatterns = [
-    path('home/', views.home)
+    path('home/', views.home),
+    path('about/', views.about),
+    path('proinfo/', views.proinfo),
+    path('showcase/', views.showcase),
+    path('survey/', views.survey),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Cory'})
+
+def about(request):
+    return render(request, 'about.html')
+
+def proinfo(request):
+    return render(request, 'proinfo.html')
+
+def showcase(request):
+    return render(request, 'showcase.html')
+
+def survey(request):
+    return render(request, 'survey.html')
